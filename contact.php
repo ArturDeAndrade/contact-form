acfe6b097d8d92577ff20ba5375edf56da30e5a8
ArturDeAndrade <adeandrade.42@gmail.com>
2020-12-14 17:30:54 -0300

    Created a simple form, styled it and coded the php that will handle everything.

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if($_POST["submit"]) {
+    $me = "arturcintra.8@gmail.com";
+    $subject = "Form to e-mail message";
+    $client = $_POST["name"];
+    $clientEmail = $_POST["email"];
+    $clientPhone = $POST["phone"];
+    $content = $_POST["msg"];
+
+    $email = "Hey there, my name is $client; $content\n\nE-mail: $clientEmail\nPhone number: $clientPhone";
+
+    mail($me, $subject, $email, "From: $client <$clientEmail>");
+}
+
+?>
