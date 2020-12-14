06dc34db3c079f6380ce04b1740936056920dc4c
ArturDeAndrade <adeandrade.42@gmail.com>
2020-12-14 19:01:22 -0300

    Corrected my mistakes, everything ~should~ be okay now.

--- contact.php
+++ contact.php
@@ -10,21 +10,7 @@ if($_POST["submit"]) {
     $email = "Hey there, my name is $client; $content\n\nE-mail: $clientEmail\nPhone number: $clientPhone";
 
     mail($me, $subject, $email, "From: $client <$clientEmail>");
+
+    header(‘Location: index.html’);
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Proza+Libre&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="basic.css">
-    <title>Form Submitted</title>
-</head>
-<body>
-    <h1>Your message has been sent!</h1>
-    <h2>Thank you for your time, we'll answer your message as soon as possible.</h2>
-    <a href="./index.html">GO BACK</a>
-</body>
-</html>
